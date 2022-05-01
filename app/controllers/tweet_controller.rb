3c676ad29eb11b111ae259b73450d72345256749
tetsu-26 <heiwa326427@gmail.com>
2022-05-01 21:18:47 +0900

    DB_save

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,4 +1,14 @@
 class TweetController < ApplicationController
   def index
   end
+
+  def new
+
+  end
+
+  def create
+    Tweet.create(params.permit(:image, :text))
+    redirect_to root_path
+  end
+
 end
